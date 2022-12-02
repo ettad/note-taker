be8cdee6608bf8bfa925d246624853a5014503c0
Tadesse Tilahun <ettad2@gmail.com>
2022-12-01 22:04:42 -0700

    Routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+const app = require('express').Router();
+const { link } = require('fs');
+const path = require('path');
+
+
+app.get('/notes', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+
+})
+
+app.get('*', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+
+});
+
+module.exports = app;
+
